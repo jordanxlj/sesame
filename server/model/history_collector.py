8cc6d0649fb6ac66d2ba62af386034816704207f
jordan <lijun.xue.jordan@gmail.com>
2015-11-14 23:44:51 +0800

    refactoring compare data into history collector.

--- server/model/history_collector.py
+++ server/model/history_collector.py
@@ -1,6 +1,17 @@
 import json
+from datetime import datetime
 
 from util.util import Util
+
+def data_cmp(data1, data2):
+    day1 = datetime.strptime(data1[0], '%Y-%m-%d')
+    day2 = datetime.strptime(data2[0], '%Y-%m-%d')
+    if day1 > day2:
+        return 1
+    elif day1 < day2:
+        return -1
+    else:
+        return 0
 
 class StockHistoryCollector(object):
     def __init__(self):
@@ -18,17 +29,19 @@ class StockHistoryCollector(object):
             close_price = float(data[2])
             updown = float(data[4].strip('%'))
             turnover = float(data[9].strip('%'))
-            result[day] = [close_price, updown, turnover]
+            result[day] = {'close': close_price, 'updown': updown, 'turnover': turnover}
 
     def collect_history_data(self, stock_id):
         history_datas = {}
         history_datas['code'] = stock_id
 
         url = self.__get_history_url(stock_id)
-        print url
         content = Util.http_get_content(url=url)
         self.__parse_data(content, history_datas)
-        return history_datas
+
+        data = list((k, v) for (k, v) in history_datas.iteritems() if k.startswith('2015'))
+        data.sort(data_cmp)
+        return data
 
 if __name__ == "__main__":
     collector = StockHistoryCollector()
